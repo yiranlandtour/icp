66b6fc3206839aa2aca022c3e4ffdfedceb46af5
yiranlandtour <yiranlandtour@163.com>
2022-06-05 22:23:23 +0800

    add transfer

--- tao/src/tao/src/lib.rs
+++ tao/src/tao/src/lib.rs
@@ -1,4 +1,598 @@
-#[ic_cdk_macros::query]
-fn greet(name: String) -> String {
-    format!("Hello, {}!", name)
+use std::cell::RefCell;
+use std::hash::Hash;
+use candid::{candid_method, CandidType, Principal};
+
+use ic_cdk_macros::*;
+use ic_ledger_types::{AccountIdentifier, BlockIndex, DEFAULT_SUBACCOUNT, MAINNET_LEDGER_CANISTER_ID, Memo, Subaccount, Tokens};
+use serde::{Deserialize, Serialize};
+
+#[derive(CandidType, Serialize, Deserialize, Clone, Debug, Hash, PartialEq)]
+pub struct Conf {
+    ledger_canister_id: Principal,
+    // The subaccount of the account identifier that will be used to withdraw tokens and send them
+    // to another account identifier. If set to None then the default subaccount will be used.
+    // See the [Ledger doc](https://smartcontracts.org/docs/integration/ledger-quick-start.html#_accounts).
+    subaccount: Option<Subaccount>,
+    transaction_fee: Tokens
+}
+
+impl Default for Conf {
+    fn default() -> Self {
+        Conf {
+            ledger_canister_id: MAINNET_LEDGER_CANISTER_ID,
+            subaccount: None,
+            transaction_fee: Tokens::from_e8s(10_000),
+        }
+    }
+}
+
+thread_local! {
+    static CONF: RefCell<Conf> = RefCell::new(Conf::default());
+}
+
+#[init]
+#[candid_method(init)]
+fn init(conf: Conf) {
+    CONF.with(|c| c.replace(conf));
+}
+
+#[derive(CandidType, Serialize, Deserialize, Clone, Debug, Hash)]
+pub struct TransferArgs {
+    amount: Tokens,
+    to_principal: Principal,
+    to_subaccount: Option<Subaccount>,
+}
+
+#[update]
+#[candid_method(update)]
+async fn transfer(args: TransferArgs) -> Result<BlockIndex, String> {
+    ic_cdk::println!("Transferring {} tokens to principal {} subaccount {:?}", &args.amount, &args.to_principal, &args.to_subaccount);
+    let ledger_canister_id = CONF.with(|conf| conf.borrow().ledger_canister_id);
+    let to_subaccount = args.to_subaccount.unwrap_or(DEFAULT_SUBACCOUNT);
+    let transfer_args = CONF.with(|conf| {
+        let conf = conf.borrow();
+        ic_ledger_types::TransferArgs {
+            memo: Memo(0),
+            amount: args.amount,
+            fee: conf.transaction_fee,
+            from_subaccount: conf.subaccount,
+            to: AccountIdentifier::new(&args.to_principal, &to_subaccount),
+            created_at_time: None,
+        }
+    });
+    ic_ledger_types::transfer(ledger_canister_id, transfer_args).await
+        .map_err(|e| format!("failed to call ledger: {:?}", e))?
+        .map_err(|e| format!("ledger transfer error {:?}", e))
+}
+
+
+mod http;
+
+const MGMT: Principal = Principal::from_slice(&[]);
+
+thread_local! {
+    static STATE: RefCell<State> = RefCell::default();
+}
+
+#[derive(CandidType, Deserialize)]
+struct StableState {
+    state: State,
+    hashes: Vec<(String, Hash)>,
+}
+
+#[pre_upgrade]
+fn pre_upgrade() {
+    let state = STATE.with(|state| mem::take(&mut *state.borrow_mut()));
+    let hashes = http::HASHES.with(|hashes| mem::take(&mut *hashes.borrow_mut()));
+    let hashes = hashes.iter().map(|(k, v)| (k.clone(), *v)).collect();
+    let stable_state = StableState { state, hashes };
+    storage::stable_save((stable_state,)).unwrap();
+}
+#[post_upgrade]
+fn post_upgrade() {
+    let (StableState { state, hashes },) = storage::stable_restore().unwrap();
+    STATE.with(|state0| *state0.borrow_mut() = state);
+    let hashes = hashes.into_iter().collect();
+    http::HASHES.with(|hashes0| *hashes0.borrow_mut() = hashes);
+}
+
+#[derive(CandidType, Deserialize)]
+struct InitArgs {
+    custodians: Option<HashSet<Principal>>,
+    logo: Option<LogoResult>,
+    name: String,
+    symbol: String,
+}
+
+#[init]
+fn init(args: InitArgs) {
+    STATE.with(|state| {
+        let mut state = state.borrow_mut();
+        state.custodians = args
+            .custodians
+            .unwrap_or_else(|| HashSet::from_iter([api::caller()]));
+        state.name = args.name;
+        state.symbol = args.symbol;
+        state.logo = args.logo;
+    });
+}
+
+#[derive(CandidType, Deserialize)]
+enum Error {
+    Unauthorized,
+    InvalidTokenId,
+    ZeroAddress,
+    Other,
+}
+
+impl From<TryFromIntError> for Error {
+    fn from(_: TryFromIntError) -> Self {
+        Self::InvalidTokenId
+    }
+}
+
+type Result<T = u128, E = Error> = StdResult<T, E>;
+
+// --------------
+// base interface
+// --------------
+
+#[query(name = "balanceOfDip721")]
+fn balance_of(user: Principal) -> u64 {
+    STATE.with(|state| {
+        state
+            .borrow()
+            .nfts
+            .iter()
+            .filter(|n| n.owner == user)
+            .count() as u64
+    })
+}
+
+#[query(name = "ownerOfDip721")]
+fn owner_of(token_id: u64) -> Result<Principal> {
+    STATE.with(|state| {
+        let owner = state
+            .borrow()
+            .nfts
+            .get(usize::try_from(token_id)?)
+            .ok_or(Error::InvalidTokenId)?
+            .owner;
+        Ok(owner)
+    })
+}
+
+#[update(name = "transferFromDip721")]
+fn transfer_from(from: Principal, to: Principal, token_id: u64) -> Result {
+    STATE.with(|state| {
+        let mut state = state.borrow_mut();
+        let state = &mut *state;
+        let nft = state
+            .nfts
+            .get_mut(usize::try_from(token_id)?)
+            .ok_or(Error::InvalidTokenId)?;
+        let caller = api::caller();
+        if nft.owner != caller
+            && nft.approved != Some(caller)
+            && !state
+                .operators
+                .get(&from)
+                .map(|s| s.contains(&caller))
+                .unwrap_or(false)
+            && !state.custodians.contains(&caller)
+        {
+            Err(Error::Unauthorized)
+        } else if nft.owner != from {
+            Err(Error::Other)
+        } else {
+            nft.approved = None;
+            nft.owner = to;
+            Ok(state.next_txid())
+        }
+    })
+}
+
+#[update(name = "safeTransferFromDip721")]
+fn safe_transfer_from(from: Principal, to: Principal, token_id: u64) -> Result {
+    if to == MGMT {
+        Err(Error::ZeroAddress)
+    } else {
+        transfer_from(from, to, token_id)
+    }
+}
+
+#[query(name = "supportedInterfacesDip721")]
+fn supported_interfaces() -> &'static [InterfaceId] {
+    &[
+        InterfaceId::TransferNotification,
+        // InterfaceId::Approval, // Psychedelic/DIP721#5
+        InterfaceId::Burn,
+        InterfaceId::Mint,
+    ]
+}
+
+#[derive(CandidType, Deserialize, Clone)]
+struct LogoResult {
+    logo_type: Cow<'static, str>,
+    data: Cow<'static, str>,
+}
+
+#[export_name = "canister_query logoDip721"]
+fn logo() /* -> &'static LogoResult */
+{
+    ic_cdk::setup();
+    STATE.with(|state| call::reply((state.borrow().logo.as_ref().unwrap_or(&DEFAULT_LOGO),)))
+}
+
+#[query(name = "nameDip721")]
+fn name() -> String {
+    STATE.with(|state| state.borrow().name.clone())
+}
+
+#[query(name = "symbolDip721")]
+fn symbol() -> String {
+    STATE.with(|state| state.borrow().symbol.clone())
+}
+
+const DEFAULT_LOGO: LogoResult = LogoResult {
+    data: Cow::Borrowed(include_base64!("logo.png")),
+    logo_type: Cow::Borrowed("image/png"),
+};
+
+#[query(name = "totalSupplyDip721")]
+fn total_supply() -> u64 {
+    STATE.with(|state| state.borrow().nfts.len() as u64)
+}
+
+#[export_name = "canister_query getMetadataDip721"]
+fn get_metadata(/* token_id: u64 */) /* -> Result<&'static MetadataDesc> */
+{
+    ic_cdk::setup();
+    let token_id = call::arg_data::<(u64,)>().0;
+    let res: Result<()> = STATE.with(|state| {
+        let state = state.borrow();
+        let metadata = &state
+            .nfts
+            .get(usize::try_from(token_id)?)
+            .ok_or(Error::InvalidTokenId)?
+            .metadata;
+        call::reply((Ok::<_, Error>(metadata),));
+        Ok(())
+    });
+    if let Err(e) = res {
+        call::reply((Err::<MetadataDesc, _>(e),));
+    }
+}
+
+#[derive(CandidType)]
+struct ExtendedMetadataResult<'a> {
+    metadata_desc: MetadataDescRef<'a>,
+    token_id: u64,
+}
+
+#[export_name = "canister_update getMetadataForUserDip721"]
+fn get_metadata_for_user(/* user: Principal */) /* -> Vec<ExtendedMetadataResult> */
+{
+    ic_cdk::setup();
+    let user = call::arg_data::<(Principal,)>().0;
+    STATE.with(|state| {
+        let state = state.borrow();
+        let metadata: Vec<_> = state
+            .nfts
+            .iter()
+            .filter(|n| n.owner == user)
+            .map(|n| ExtendedMetadataResult {
+                metadata_desc: &n.metadata,
+                token_id: n.id,
+            })
+            .collect();
+        call::reply((metadata,));
+    });
+}
+
+// ----------------------
+// notification interface
+// ----------------------
+
+#[update(name = "transferFromNotifyDip721")]
+fn transfer_from_notify(from: Principal, to: Principal, token_id: u64, data: Vec<u8>) -> Result {
+    let res = transfer_from(from, to, token_id)?;
+    if let Ok(arg) = Encode!(&api::caller(), &from, &token_id, &data) {
+        // Using call_raw ensures we don't need to await the future for the call to be executed.
+        // Calling an arbitrary function like this means that a malicious recipient could call 
+        // transferFromNotifyDip721 in their onDIP721Received function, resulting in an infinite loop.
+        // This will trap eventually, but the transfer will have already been completed and the state-change persisted.
+        // That means the original transfer must reply before that happens, or the caller will be
+        // convinced that the transfer failed when it actually succeeded. So we don't await the call,
+        // so that we'll reply immediately regardless of how long the notification call takes.
+        let _ = api::call::call_raw(to, "onDIP721Received", arg, 0);
+    }
+    Ok(res)
+}
+
+#[update(name = "safeTransferFromNotifyDip721")]
+fn safe_transfer_from_notify(
+    from: Principal,
+    to: Principal,
+    token_id: u64,
+    data: Vec<u8>,
+) -> Result {
+    if to == MGMT {
+        Err(Error::ZeroAddress)
+    } else {
+        transfer_from_notify(from, to, token_id, data)
+    }
+}
+
+// ------------------
+// approval interface
+// ------------------
+
+#[update(name = "approveDip721")]
+fn approve(user: Principal, token_id: u64) -> Result {
+    STATE.with(|state| {
+        let mut state = state.borrow_mut();
+        let state = &mut *state;
+        let caller = api::caller();
+        let nft = state
+            .nfts
+            .get_mut(usize::try_from(token_id)?)
+            .ok_or(Error::InvalidTokenId)?;
+        if nft.owner != caller
+            && nft.approved != Some(caller)
+            && !state
+                .operators
+                .get(&user)
+                .map(|s| s.contains(&caller))
+                .unwrap_or(false)
+            && !state.custodians.contains(&caller)
+        {
+            Err(Error::Unauthorized)
+        } else {
+            nft.approved = Some(user);
+            Ok(state.next_txid())
+        }
+    })
+}
+
+#[update(name = "setApprovalForAllDip721")]
+fn set_approval_for_all(operator: Principal, is_approved: bool) -> Result {
+    STATE.with(|state| {
+        let mut state = state.borrow_mut();
+        let caller = api::caller();
+        if operator != caller {
+            let operators = state.operators.entry(caller).or_default();
+            if operator == MGMT {
+                if !is_approved {
+                    operators.clear();
+                } else {
+                    // cannot enable everyone as an operator
+                }
+            } else {
+                if is_approved {
+                    operators.insert(operator);
+                } else {
+                    operators.remove(&operator);
+                }
+            }
+        }
+        Ok(state.next_txid())
+    })
+}
+
+// #[query(name = "getApprovedDip721")] // Psychedelic/DIP721#5
+fn _get_approved(token_id: u64) -> Result<Principal> {
+    STATE.with(|state| {
+        let approved = state
+            .borrow()
+            .nfts
+            .get(usize::try_from(token_id)?)
+            .ok_or(Error::InvalidTokenId)?
+            .approved
+            .unwrap_or_else(api::caller);
+        Ok(approved)
+    })
+}
+
+#[query(name = "isApprovedForAllDip721")]
+fn is_approved_for_all(operator: Principal) -> bool {
+    STATE.with(|state| {
+        state
+            .borrow()
+            .operators
+            .get(&api::caller())
+            .map(|s| s.contains(&operator))
+            .unwrap_or(false)
+    })
+}
+
+// --------------
+// mint interface
+// --------------
+
+#[update(name = "mintDip721")]
+fn mint(
+    to: Principal,
+    metadata: MetadataDesc,
+    blob_content: Vec<u8>,
+) -> Result<MintResult, ConstrainedError> {
+    let (txid, tkid) = STATE.with(|state| {
+        let mut state = state.borrow_mut();
+        if !state.custodians.contains(&api::caller()) {
+            return Err(ConstrainedError::Unauthorized);
+        }
+        let new_id = state.nfts.len() as u64;
+        let nft = Nft {
+            owner: to,
+            approved: None,
+            id: new_id,
+            metadata,
+            content: blob_content,
+        };
+        state.nfts.push(nft);
+        Ok((state.next_txid(), new_id))
+    })?;
+    http::add_hash(tkid);
+    Ok(MintResult {
+        id: txid,
+        token_id: tkid,
+    })
+}
+
+// --------------
+// burn interface
+// --------------
+
+#[update(name = "burnDip721")]
+fn burn(token_id: u64) -> Result {
+    STATE.with(|state| {
+        let mut state = state.borrow_mut();
+        let nft = state
+            .nfts
+            .get_mut(usize::try_from(token_id)?)
+            .ok_or(Error::InvalidTokenId)?;
+        if nft.owner != api::caller() {
+            Err(Error::Unauthorized)
+        } else {
+            nft.owner = MGMT;
+            Ok(state.next_txid())
+        }
+    })
+}
+
+#[derive(CandidType, Deserialize, Default)]
+struct State {
+    nfts: Vec<Nft>,
+    custodians: HashSet<Principal>,
+    operators: HashMap<Principal, HashSet<Principal>>, // owner to operators
+    logo: Option<LogoResult>,
+    name: String,
+    symbol: String,
+    txid: u128,
+}
+
+#[derive(CandidType, Deserialize)]
+struct Nft {
+    owner: Principal,
+    approved: Option<Principal>,
+    id: u64,
+    metadata: MetadataDesc,
+    content: Vec<u8>,
+}
+
+type MetadataDesc = Vec<MetadataPart>;
+type MetadataDescRef<'a> = &'a [MetadataPart];
+
+#[derive(CandidType, Deserialize)]
+struct MetadataPart {
+    purpose: MetadataPurpose,
+    key_val_data: HashMap<String, MetadataVal>,
+    data: Vec<u8>,
+}
+
+#[derive(CandidType, Deserialize, PartialEq)]
+enum MetadataPurpose {
+    Preview,
+    Rendered,
+}
+
+#[derive(CandidType, Deserialize)]
+struct MintResult {
+    token_id: u64,
+    id: u128,
+}
+
+#[allow(clippy::enum_variant_names)]
+#[derive(CandidType, Deserialize)]
+enum MetadataVal {
+    TextContent(String),
+    BlobContent(Vec<u8>),
+    NatContent(u128),
+    Nat8Content(u8),
+    Nat16Content(u16),
+    Nat32Content(u32),
+    Nat64Content(u64),
+}
+
+impl State {
+    fn next_txid(&mut self) -> u128 {
+        let txid = self.txid;
+        self.txid += 1;
+        txid
+    }
+}
+
+#[derive(CandidType, Deserialize)]
+enum InterfaceId {
+    Approval,
+    TransactionHistory,
+    Mint,
+    Burn,
+    TransferNotification,
+}
+
+#[derive(CandidType, Deserialize)]
+enum ConstrainedError {
+    Unauthorized,
+}
+
+#[update]
+fn set_name(name: String) -> Result<()> {
+    STATE.with(|state| {
+        let mut state = state.borrow_mut();
+        if state.custodians.contains(&api::caller()) {
+            state.name = name;
+            Ok(())
+        } else {
+            Err(Error::Unauthorized)
+        }
+    })
+}
+
+#[update]
+fn set_symbol(sym: String) -> Result<()> {
+    STATE.with(|state| {
+        let mut state = state.borrow_mut();
+        if state.custodians.contains(&api::caller()) {
+            state.symbol = sym;
+            Ok(())
+        } else {
+            Err(Error::Unauthorized)
+        }
+    })
+}
+
+#[update]
+fn set_logo(logo: Option<LogoResult>) -> Result<()> {
+    STATE.with(|state| {
+        let mut state = state.borrow_mut();
+        if state.custodians.contains(&api::caller()) {
+            state.logo = logo;
+            Ok(())
+        } else {
+            Err(Error::Unauthorized)
+        }
+    })
+}
+
+#[update]
+fn set_custodian(user: Principal, custodian: bool) -> Result<()> {
+    STATE.with(|state| {
+        let mut state = state.borrow_mut();
+        if state.custodians.contains(&api::caller()) {
+            if custodian {
+                state.custodians.insert(user);
+            } else {
+                state.custodians.remove(&user);
+            }
+            Ok(())
+        } else {
+            Err(Error::Unauthorized)
+        }
+    })
+}
+
+#[query]
+fn is_custodian(principal: Principal) -> bool {
+    STATE.with(|state| state.borrow().custodians.contains(&principal))
 }
